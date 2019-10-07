d39008d9e17a3222c3b7a78cbd30d55ce5fff419
Leroy Vargis <mail@leroyjv.dev>
2019-10-06 18:24:07 -0700

    remove stubbed data

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -4,7 +4,7 @@ import { SearchService } from './search.service';
 @Component({
   selector: 'ngx-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
   data: Ad[];
@@ -12,64 +12,6 @@ export class SearchComponent implements OnInit {
   constructor(private searchService: SearchService) { }
 
   ngOnInit() {
-    // this.data = [
-    //   {
-    //     title: 'Web Site Firewall',
-    //     content: 'Complete Website Protection from Barracuda. Request Eval Unit Today!',
-    //     company: 'barracuda.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: 'Create Your Own Website',
-    //     content: '10,000+ Templates for Your Website. Free Domain & Hosting. Start Now!',
-    //     company: 'web.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: '$0.99 Domains at Go Daddy - Why Pay More? Compare Us!',
-    //     content: 'Free Hosting w/Site Builder & More.',
-    //     company: 'GoDaddy.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: '$0.99 Domains at Go Daddy - Why Pay More? Compare Us!',
-    //     content: 'Free Hosting w/Site Builder & More.',
-    //     company: 'GoDaddy.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: 'Web Site Firewall',
-    //     content: 'Complete Website Protection from Barracuda. Request Eval Unit Today!',
-    //     company: 'barracuda.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: 'Create Your Own Website',
-    //     content: '10,000+ Templates for Your Website. Free Domain & Hosting. Start Now!',
-    //     company: 'web.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: 'Create Your Own Website',
-    //     content: '10,000+ Templates for Your Website. Free Domain & Hosting. Start Now!',
-    //     company: 'web.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    //   {
-    //     title: 'Web Site Firewall',
-    //     content: 'Complete Website Protection from Barracuda. Request Eval Unit Today!',
-    //     company: 'barracuda.com',
-    //     first_seen: '2014-04-14',
-    //     last_seen: '2014-04-14',
-    //   },
-    // ];
   }
 
   onSearchBtnClk() {
