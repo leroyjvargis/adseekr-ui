54d4f3632af2ead3fc5871295261f4cd0a4b5486
Leroy Vargis <mail@leroyjv.dev>
2019-10-06 17:50:48 -0700

    minor change

--- src/app/home/side-menu-items.ts
+++ src/app/home/side-menu-items.ts
@@ -8,31 +8,31 @@ export const MENU_ITEMS: NbMenuItem[] = [
     home: true,
 
   },
-  {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-    title: 'UI Features',
-    icon: 'keypad-outline',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-    ],
-  },
+  // {
+  //   title: 'FEATURES',
+  //   group: true,
+  // },
+  // {
+  //   title: 'UI Features',
+  //   icon: 'keypad-outline',
+  //   link: '/pages/ui-features',
+  //   children: [
+  //     {
+  //       title: 'Grid',
+  //       link: '/pages/ui-features/grid',
+  //     },
+  //     {
+  //       title: 'Icons',
+  //       link: '/pages/ui-features/icons',
+  //     },
+  //     {
+  //       title: 'Typography',
+  //       link: '/pages/ui-features/typography',
+  //     },
+  //     {
+  //       title: 'Animated Searches',
+  //       link: '/pages/ui-features/search-fields',
+  //     },
+  //   ],
+  // },
 ];
